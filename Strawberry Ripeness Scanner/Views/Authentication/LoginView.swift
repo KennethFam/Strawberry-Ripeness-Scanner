11c986e2c00b65c6e9ed40b61350967c9501b7f1
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-03-25 09:33:20 -0700

    Update LoginView.swift

--- Strawberry Ripeness Scanner/Views/Authentication/LoginView.swift
+++ Strawberry Ripeness Scanner/Views/Authentication/LoginView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct LoginView: View {
     @State private var email = ""
@@ -16,83 +17,90 @@ struct LoginView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                // Image
-                Image("LoginIcon")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 100, height: 120)
-                    .padding(.vertical, 32)
-                
-                // Form Fields
-                VStack(spacing: 24) {
-                    InputView(text: $email,
-                              title: "Email",
-                              placeholder: "name@example.com")
-                    .autocapitalization(.none)
-                    .onChange(of: email) {
-                        goodEmail = validEmail(email)
+            ZStack {
+                VStack {
+                    // Image
+                    Image("LoginIcon")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 120)
+                        .padding(.vertical, 32)
+                    
+                    // Form Fields
+                    VStack(spacing: 24) {
+                        InputView(text: $email,
+                                  title: "Email",
+                                  placeholder: "name@example.com")
+                        .autocapitalization(.none)
+                        .onChange(of: email) {
+                            goodEmail = validEmail(email)
+                        }
+                        if !email.isEmpty && !goodEmail {
+                            Text("Invalid Email")
+                                .foregroundColor(Color(.systemRed))
+                                .font(.subheadline)
+                                .frame(maxWidth: UIScreen.main.bounds.width - 32, alignment: .leading)
+                                .padding(.top, -20)
+                        }
+                        
+                        InputView(text: $password,
+                                  title: "Password",
+                                  placeholder: "Enter your password",
+                                  isSecureField: true)
+                        if viewModel.cloudEnabledStatus == false {
+                            Text("Server is currently down for maintenance.")
+                                .foregroundColor(Color(.systemRed))
+                                .font(.subheadline)
+                                .frame(maxWidth: UIScreen.main.bounds.width - 32, alignment: .leading)
+                                .padding(.top, -20)
+                                .padding(.bottom, -20)
+                        }
                     }
-                    if !email.isEmpty && !goodEmail {
-                        Text("Invalid Email")
-                            .foregroundColor(Color(.systemRed))
-                            .font(.subheadline)
-                            .frame(maxWidth: UIScreen.main.bounds.width - 32, alignment: .leading)
-                            .padding(.top, -20)
+                    .padding(.horizontal)
+                    .padding(.top, 12)
+                    
+                    // Login Button
+                    Button {
+                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        viewModel.loading = true
+                        Task {
+                            try await viewModel.signIn(withEmail: email,
+                                                       password: password)
+                        }
+                    } label: {
+                        HStack {
+                            Text("LOGIN")
+                                .fontWeight(.semibold)
+                        }
+                        .foregroundColor(Color(.white))
+                        // takes width of screen - 32 pixels
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                    }
+                    .background(Color(.systemBlue))
+                    .cornerRadius(10)
+                    .padding(.top, 24)
+                    .disabled(!formIsValid)
+                    .opacity(formIsValid ? 1.0 : 0.5)
+                    
+                    
+                    // Sign-Up Button
+                    NavigationLink {
+                        RegistrationView()
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        HStack(spacing: 3) {
+                            Text("Dont have an account?")
+                            Text("Sign Up")
+                                .fontWeight(.bold)
+                        }
+                        .font(.system(size: 14))
                     }
                     
-                    InputView(text: $password,
-                              title: "Password",
-                              placeholder: "Enter your password",
-                              isSecureField: true)
-                    if viewModel.cloudEnabledStatus == false {
-                        Text("Server is currently down for maintenance.")
-                            .foregroundColor(Color(.systemRed))
-                            .font(.subheadline)
-                            .frame(maxWidth: UIScreen.main.bounds.width - 32, alignment: .leading)
-                            .padding(.top, -20)
-                            .padding(.bottom, -20)
-                    }
+                    Spacer()
                 }
-                .padding(.horizontal)
-                .padding(.top, 12)
-                
-                // Login Button
-                Button {
-                    Task {
-                        try await viewModel.signIn(withEmail: email,
-                                                   password: password)
-                    }
-                } label: {
-                    HStack {
-                        Text("LOGIN")
-                            .fontWeight(.semibold)
-                    }
-                    .foregroundColor(Color(.white))
-                    // takes width of screen - 32 pixels
-                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                if viewModel.loading {
+                    LoadingView(text: "Signing in...")
                 }
-                .background(Color(.systemBlue))
-                .cornerRadius(10)
-                .padding(.top, 24)
-                .disabled(!formIsValid)
-                .opacity(formIsValid ? 1.0 : 0.5)
-
-                
-                // Sign-Up Button
-                NavigationLink {
-                    RegistrationView()
-                        .navigationBarBackButtonHidden(true)
-                } label: {
-                    HStack(spacing: 3) {
-                        Text("Dont have an account?")
-                        Text("Sign Up")
-                            .fontWeight(.bold)
-                    }
-                    .font(.system(size: 14))
-                }
-                
-                Spacer()
             }
         }
     }
