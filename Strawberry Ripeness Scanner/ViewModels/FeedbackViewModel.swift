956f119c451e172e4fa4ab3894fea3fbdcef893c
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-03-28 20:58:00 -0700

    Add files via upload

---
+++ Strawberry Ripeness Scanner/ViewModels/FeedbackViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  FeedbackViewModel.swift
+//  Strawberry Ripeness Scanner
+//
+//  Created by Kenneth Pham on 3/27/25.
+//
+
+import SwiftUI
+import FirebaseStorage
+
+
+enum ScanPath {
+    case feedback
+}
+
+class FeedbackViewModel: ObservableObject {
+    @Published var path = [ScanPath]()
+    @Published var loading = false
+    
+    func uploadFeedback(_ image: MyImage, feedback: String, email: String, userID: String, completion: (() -> Void)? = nil) {
+        let feedbackID = UUID()
+        loading = true
+        // create storage reference
+        let storage = Storage.storage().reference()
+        
+        // turn image into data
+        let imageData = image.image.jpegData(compressionQuality: 0.8)
+        
+        guard imageData != nil else {
+            return
+        }
+        
+        //specify the file path and name
+        let fileRef = storage.child("Feedback/\(feedbackID).jpg")
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        metadata.customMetadata = [
+            "User ID": userID,
+            "Email": email,
+            "Image ID": "\(image.id)",
+            "feedback": feedback
+        ]
+
+        
+        let _ = fileRef.putData(imageData!, metadata: metadata) { (metadata, error) in
+            // check for errors
+            if error == nil && metadata != nil {
+                print("Feedback submitted successfully!\n")
+            }
+            // .putData is asynchronous so use completion function to signify uploadPhoto is done and to execute its completion
+            completion?()
+        }
+    }
+}
