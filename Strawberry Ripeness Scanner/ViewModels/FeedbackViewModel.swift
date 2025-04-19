d7173627654412053c48d3336264497ab27ea803
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-04-18 23:08:12 -0700

    Update FeedbackViewModel.swift

--- Strawberry Ripeness Scanner/ViewModels/FeedbackViewModel.swift
+++ Strawberry Ripeness Scanner/ViewModels/FeedbackViewModel.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseStorage
+import FirebaseFirestore
 
 class FeedbackViewModel: ObservableObject {
     @Published var loading = false
@@ -31,11 +32,12 @@ class FeedbackViewModel: ObservableObject {
         let metadata = StorageMetadata()
         metadata.contentType = "image/jpeg"
         metadata.customMetadata = [
+            "Feedback ID": "\(feedbackID)",
             "User ID": userID,
             "Email": email,
             "Image ID": "\(image.id)",
-            "Feedback": feedback.trimmingCharacters(in: .whitespacesAndNewlines),
             "Feedback Date": "\(feedbackDate)",
+            "Feedback": feedback.trimmingCharacters(in: .whitespacesAndNewlines),
             "App Version": AppData.version,
             "Upload Date": "\(image.date)",
             "Ripe": String(image.ripe),
@@ -51,6 +53,27 @@ class FeedbackViewModel: ObservableObject {
             }
             // .putData is asynchronous so use completion function to signify uploadPhoto is done and to execute its completion
             completion?()
+        }
+    }
+    
+    func uploadIssue(_ issue: String, subject: String, email: String, userID: String) async throws {
+        let ticketID = UUID()
+        let ticketDate = Date()
+        let db = Firestore.firestore()
+        
+        do {
+            try await db.collection("support_tickets").document("\(ticketID)").setData([
+            "Ticket ID": "\(ticketID)",
+            "User ID": userID,
+            "Email": email,
+            "Ticket Date": "\(ticketDate)",
+            "Subject": subject,
+            "Issue": issue,
+            "App Version": AppData.version
+            ])
+            print("Document successfully written!")
+        } catch {
+            print("Error writing document: \(error)")
         }
     }
 }
