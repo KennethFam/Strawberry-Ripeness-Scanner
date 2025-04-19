3dd2ba93b13173349c9653b1e95082b8c5be8a95
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-04-18 23:05:08 -0700

    Add files via upload

---
+++ Strawberry Ripeness Scanner/Views/Support/RipenessGuideView.swift
@@ -0,0 +1,14 @@
+//
+//  RipenessGuideView.swift
+//  Strawberry Ripeness Scanner
+//
+//  Created by Kenneth Pham on 4/16/25.
+//
+
+import SwiftUI
+
+struct RipenessGuideView: View {
+    var body: some View {
+        Text("Ripeness Guide View")
+    }
+}
---
+++ Strawberry Ripeness Scanner/Views/Support/TutorialView.swift
@@ -0,0 +1,14 @@
+//
+//  TutorialView.swift
+//  Strawberry Ripeness Scanner
+//
+//  Created by Kenneth Pham on 4/16/25.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text("Tutorial View")
+    }
+}
