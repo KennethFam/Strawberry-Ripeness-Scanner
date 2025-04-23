fe7c5bd02a7e9680b9e56b1825e3c9f57d21fa1b
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-04-22 18:40:51 -0700

    Update RipenessGuideView.swift

--- Strawberry Ripeness Scanner/Views/Support/RipenessGuideView.swift
+++ Strawberry Ripeness Scanner/Views/Support/RipenessGuideView.swift
@@ -8,7 +8,44 @@
 import SwiftUI
 
 struct RipenessGuideView: View {
+    @Binding var path: [ScanPath]
+    let ripeText = """
+    A ripe strawberry is good to eat and good to harvest and ship. Beware, there is a chance that a ripe strawberry may become overripe or rotten during transit.
+    """
+    let nearlyRipeText = """
+    A nearly ripe strawberry is not yet good to eat but good to harvest and ship.
+    """
+    let unripeText = """
+    An unripe strawberry is not yet good to eat or harvest and ship.
+    """
+    let rottenText = """
+    A rotten strawberry is not good to eat or harvest and ship. It should be disposed of.
+    """
     var body: some View {
-        Text("Ripeness Guide View")
+        ScrollView { // makes view scrollable
+            VStack(spacing: 10){
+                PageTitle(title: "Ripeness Guide")
+                
+                Guide(className: "Ripe", text: ripeText, image: "ripe")
+                Guide(className: "Nearly Ripe", text: nearlyRipeText, image: "nearly_ripe")
+                Guide(className: "Unripe", text: unripeText, image: "unripe")
+                Guide(className: "Rotten", text: rottenText, image: "rotten")
+                
+                NavigationLink(value: ScanPath.tutorial) {
+                    HStack(spacing: 3) {
+                        Text("Dont know how to use the app?")
+                        Text("View the tutorial!")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
+
+//#Preview {
+//    RipenessGuideView()
+//}
