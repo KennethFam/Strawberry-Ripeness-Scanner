4288baf4c0a472d5305c023df13c1c10c6c08e95
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-04-22 18:41:09 -0700

    Update TutorialView.swift

--- Strawberry Ripeness Scanner/Views/Support/TutorialView.swift
+++ Strawberry Ripeness Scanner/Views/Support/TutorialView.swift
@@ -8,7 +8,18 @@
 import SwiftUI
 
 struct TutorialView: View {
+    @Binding var path: [ScanPath]
     var body: some View {
-        Text("Tutorial View")
+        VStack(spacing: 10) {
+            PageTitle(title: "Tutorial View")
+            NavigationLink(value: ScanPath.ripenessGuide) {
+                HStack(spacing: 3) {
+                    Text("Ripeness Guide")
+                        .fontWeight(.bold)
+                }
+                .font(.system(size: 14))
+            }
+            Spacer()
+        }
     }
 }
